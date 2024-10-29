e27be96108fa3fbba162bc7d5d15204b0f73e810
ahmad-nour-haidar <ahmadnourhaidar@gmail.com>
2024-10-29 23:08:25 +0300

    🔨 Refactor code

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,21 +1,55 @@
 // src/users/user.service.ts
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UpdateUserRolesDto } from './dto/update-user-roles.dto';
 import { User } from './entities/user.entity';
+import { BcryptService } from './services/bcrypt.service';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
+    private readonly bcryptService: BcryptService,
   ) {}
 
-  async create(createUserDto: CreateUserDto) {
-    const user = this.userRepository.create(createUserDto);
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    // Check if the email or username already exists
+    const existingUser = await this.userRepository.findOne({
+      where: [
+        { email: createUserDto.email },
+        { username: createUserDto.username },
+      ],
+    });
+
+    // If the user exists, throw a conflict exception with a specific message
+    if (existingUser) {
+      if (existingUser.email === createUserDto.email) {
+        throw new ConflictException('Email already exists'); // Email conflict
+      }
+      if (existingUser.username === createUserDto.username) {
+        throw new ConflictException('Username already exists'); // Username conflict
+      }
+    }
+
+    // Hash the password
+    const hashedPassword = await this.bcryptService.hash(
+      createUserDto.password,
+    );
+
+    // Create the user object with the hashed password
+    const user = this.userRepository.create({
+      ...createUserDto,
+      password: hashedPassword, // Store hashed password
+    });
+
     return await this.userRepository.save(user);
   }
 
