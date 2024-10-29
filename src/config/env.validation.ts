e431c28214d9f6b5534cc9b58fe4398729c774f2
ahmad-nour-haidar <ahmadnourhaidar@gmail.com>
2024-10-29 17:04:18 +0300

    ✨ Add env.validation.ts

---
+++ src/config/env.validation.ts
@@ -0,0 +1,58 @@
+import {
+  IsBoolean,
+  IsEnum,
+  IsNumber,
+  IsString,
+  validateSync,
+} from 'class-validator';
+import { plainToInstance } from 'class-transformer';
+
+enum NodeEnv {
+  Development = 'development',
+  Production = 'production',
+}
+
+export class EnvironmentVariables {
+  @IsEnum(NodeEnv)
+  NODE_ENV: NodeEnv;
+
+  @IsString()
+  JWT_SECRET: string;
+
+  @IsString()
+  JWT_EXPIRES_IN: string;
+
+  @IsString()
+  DB_HOST: string;
+
+  @IsNumber()
+  DB_PORT: number;
+
+  @IsString()
+  DB_USERNAME: string;
+
+  @IsString()
+  DB_PASSWORD: string;
+
+  @IsString()
+  DB_NAME: string;
+
+  @IsBoolean()
+  DB_SYNCHRONIZE: boolean;
+}
+
+// Validation function to ensure env values are properly validated
+export function validate(config: Record<string, unknown>) {
+  const validatedConfig = plainToInstance(EnvironmentVariables, config, {
+    enableImplicitConversion: true,
+  });
+  const errors = validateSync(validatedConfig, {
+    skipMissingProperties: false,
+  });
+
+  if (errors.length > 0) {
+    throw new Error(errors.toString());
+  }
+
+  return validatedConfig;
+}
