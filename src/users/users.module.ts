f968c07bd335f6235e5bf2140c8af400e4652c3b
ahmad-nour-haidar <ahmadnourhaidar@gmail.com>
2024-10-30 23:50:23 +0300

    🐛 Fix users.module.ts

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { BcryptService } from './services/bcrypt.service';
 import { UsersController } from './users.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
   providers: [UsersService, BcryptService],
   exports: [BcryptService],
