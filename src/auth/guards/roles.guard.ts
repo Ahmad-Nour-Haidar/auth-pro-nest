436249b7d275245c5ce4a3c69d1b654292bf8e2b
ahmad-nour-haidar <ahmadnourhaidar@gmail.com>
2024-10-30 22:12:04 +0300

    ➕ Add guards

---
+++ src/auth/guards/roles.guard.ts
@@ -0,0 +1,36 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Roles } from '../enums/roles.enum';
+import { ALLOWED_ROLES_KEY } from '../../common/decorators';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<Roles[]>(
+      ALLOWED_ROLES_KEY,
+      [context.getHandler(), context.getClass()],
+    );
+    if (!requiredRoles) {
+      return true; // No roles specified, grant access
+    }
+
+    const { user } = context.switchToHttp().getRequest();
+    if (!user || !user.roles) {
+      throw new ForbiddenException('Access denied');
+    }
+
+    const hasRole = requiredRoles.some((role) => user.roles.includes(role));
+    if (!hasRole) {
+      throw new ForbiddenException('Access denied');
+    }
+
+    return true;
+  }
+}
