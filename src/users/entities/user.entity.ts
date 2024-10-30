3e4f1de18b976e09f865f6e2edc0598d2d98832a
ahmad-nour-haidar <ahmadnourhaidar@gmail.com>
2024-10-31 00:18:52 +0300

    🔨 Refactor user.entity.ts

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -8,7 +8,7 @@ import {
 } from 'typeorm';
 
 import { Roles } from '../../auth/enums/roles.enum';
-import { LoginMethod } from '../../auth/enums/login-method.enum';
+import { CreateMethod } from '../../auth/enums/create-method.enum';
 
 @Entity('users')
 export class User {
@@ -24,16 +24,21 @@ export class User {
   @Column({ nullable: true, type: 'varchar', length: 255 })
   full_name?: string;
 
-  @Column({ type: 'varchar' })
+  @Column({ type: 'varchar', select: false })
   password: string;
 
   @Column({ type: 'enum', enum: Roles, array: true, default: [Roles.User] })
   roles: Roles[];
 
-  @Column({ type: 'enum', enum: LoginMethod, default: LoginMethod.LocalEmail })
-  login_method: LoginMethod;
+  @Column({
+    type: 'enum',
+    enum: CreateMethod,
+    default: CreateMethod.LocalEmail,
+    select: false,
+  })
+  create_method: CreateMethod;
 
-  @Column({ nullable: true, type: 'varchar', length: 255 })
+  @Column({ nullable: true, type: 'varchar', length: 255, select: false })
   verify_code?: string;
 
   @CreateDateColumn({ type: 'timestamp' })
@@ -48,15 +53,15 @@ export class User {
   @Column({ type: 'timestamp', nullable: true })
   blocked_at?: Date;
 
-  @Column({ type: 'timestamp', nullable: true })
+  @Column({ type: 'timestamp', nullable: true, select: false })
   password_changed_at?: Date;
 
-  @Column({ type: 'timestamp', nullable: true })
+  @Column({ type: 'timestamp', nullable: true, select: false })
   approved_at?: Date;
 
-  @Column({ type: 'timestamp', nullable: true })
+  @Column({ type: 'timestamp', nullable: true, select: false })
   last_login_at?: Date;
 
-  @Column({ type: 'timestamp', nullable: true })
+  @Column({ type: 'timestamp', nullable: true, select: false })
   last_logout_at?: Date;
 }
