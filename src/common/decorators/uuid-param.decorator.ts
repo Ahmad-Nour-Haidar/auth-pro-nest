b47e584eff08acb5c817ae17e4f0594dd69bb9aa
ahmad-nour-haidar <ahmadnourhaidar@gmail.com>
2024-11-02 00:22:35 +0300

    🐛 Fix bugs in uuid-param.decorator.ts

--- src/common/decorators/uuid-param.decorator.ts
+++ src/common/decorators/uuid-param.decorator.ts
@@ -3,17 +3,17 @@ import {
   createParamDecorator,
   ExecutionContext,
 } from '@nestjs/common';
-import { v4 as isUuid } from 'uuid';
+import * as uuid from 'uuid';
 
-export const UUIDParam = createParamDecorator(
+export const UUIDV4Param = createParamDecorator(
   (data: string = 'id', ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
     const param = request.params[data];
 
-    if (!isUuid(param)) {
-      throw new BadRequestException(`${data} must be a valid UUID`);
+    if (uuid.validate(param) && uuid.version(param) === 4) {
+      return param;
     }
 
-    return param;
+    throw new BadRequestException(`${data} must be a valid UUID V4`);
   },
 );
