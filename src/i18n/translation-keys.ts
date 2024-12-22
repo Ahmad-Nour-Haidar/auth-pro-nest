ac2b8ea862b0986b2985df475bf85b297752f3ad
ahmad-nour-haidar <ahmadnourhaidar@gmail.com>
2024-12-22 22:39:18 +0300

    ➕ Add translation

--- src/i18n/translation-keys.ts
+++ src/i18n/translation-keys.ts
@@ -91,4 +91,5 @@ export class TranslationKeys {
   static logged_out_after_token_issued = 'translations.logged_out_after_token_issued';
   static success = 'translations.success';
   static device_token_not_found = 'translations.device_token_not_found';
+  static logged_out_successfully = 'translations.logged_out_successfully';
 }
