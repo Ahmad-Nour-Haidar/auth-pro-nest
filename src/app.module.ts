d0206d799995db9a3de9faef46bb73b3340bd760
ahmad-nour-haidar <ahmadnourhaidar@gmail.com>
2025-01-08 22:17:41 +0300

    ✨ deployment

--- src/app.module.ts
+++ src/app.module.ts
@@ -63,16 +63,26 @@ import { ThrottlerExceptionFilter } from './common/exceptions/throttler-exceptio
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_NAME'),
-        autoLoadEntities: true,
-        synchronize: configService.get<boolean>('DB_SYNCHRONIZE'), // Set false in production
-      }),
+      useFactory: (configService: ConfigService) => {
+        const isProduction =
+          configService.get<string>('NODE_ENV') === NodeEnv.production;
+
+        return {
+          type: 'postgres',
+          host: configService.get<string>('DB_HOST'),
+          port: configService.get<number>('DB_PORT'),
+          username: configService.get<string>('DB_USERNAME'),
+          password: configService.get<string>('DB_PASSWORD'),
+          database: configService.get<string>('DB_NAME'),
+          autoLoadEntities: true,
+          synchronize: configService.get<boolean>('DB_SYNCHRONIZE'), // Set false in production
+          ...(isProduction && {
+            ssl: {
+              rejectUnauthorized: false, // Necessary for Supabase
+            },
+          }),
+        };
+      },
     }),
 
     // Rate Limiting (optional but recommended)
